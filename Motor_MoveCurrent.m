9b76159c21c5ecdef81c4509a2934f5eb177f44b
Jan Kaska <kaskaj@kte.zcu.cz>
2019-12-05 14:21:56 +0100

    Stator coils are now rotating, results agree with Comsol.

--- Motor_MoveCurrent.m
+++ Motor_MoveCurrent.m
@@ -18,21 +18,69 @@ Jc = N*Ic*S;
 J_ele = zeros(mesh.nelement,1);
 J     = zeros(mesh.npoint,1);
 
-pos = [7:43]';
-pos = circshift(pos,step);
+coil = zeros(2*36,1);
+coil(1:3)   =  1;   coil(7:9)   =  1;
+coil(13:15) = -2;	coil(19:21) = -2;
+coil(25:27) =  3;   coil(31:33) =  3;
+coil(37:39) = -1;   coil(43:45) = -1;
+coil(49:51) =  2;	coil(55:57) =  2;
+coil(61:63) = -3;   coil(67:69) = -3;
 
-ii_a = ismember(mesh.tnum, [pos(1:3),pos(7:9)]);
-ii_c = ismember(mesh.tnum, [pos(13:15),pos(19:21)]);
-ii_b = ismember(mesh.tnum, [pos(25:27),pos(31:33)]);
+pos   = (7:42);
+coil   = circshift(coil,step);
 
-J_ele(ii_a)  = 1;
-J_ele(ii_c)  = 2;
-J_ele(ii_b)  = 3;
+for i = 1:36
+   ii = ismember(mesh.tnum, pos(i));
+   J_ele(ii) = coil(i);  
+end
 
-J(mesh.elems2nodes(J_ele == 1)) =  Ja;
-J(mesh.elems2nodes(J_ele == 2)) = -Jc;
-J(mesh.elems2nodes(J_ele == 3)) =  Jb;
+J(mesh.elems2nodes(J_ele ==  1)) =  Ja;
+J(mesh.elems2nodes(J_ele == -1)) = -Ja;
+J(mesh.elems2nodes(J_ele ==  2)) =  Jc;
+J(mesh.elems2nodes(J_ele == -2)) = -Jc;
+J(mesh.elems2nodes(J_ele ==  3)) =  Jb;
+J(mesh.elems2nodes(J_ele == -3)) = -Jb;
 
+Im = params.Im;
+S  = params.S;
+N  = params.N;
+w0 = params.w0;
+
+Ia  = Im*cos(w0*t);                 
+Ib  = Im*cos(w0*t - 2*pi/3);       
+Ic  = Im*cos(w0*t + 2*pi/3);      
+
+Ja = N*Ia*S;
+Jb = N*Ib*S;
+Jc = N*Ic*S;
+
+%% Prescribe current density
+
+J_ele = zeros(mesh.nelement,1);
+J     = zeros(mesh.npoint,1);
+
+coil = zeros(2*36,1);
+coil(1:3)   =  1;   coil(7:9)   =  1;
+coil(13:15) = -2;	coil(19:21) = -2;
+coil(25:27) =  3;   coil(31:33) =  3;
+coil(37:39) = -1;   coil(43:45) = -1;
+coil(49:51) =  2;	coil(55:57) =  2;
+coil(61:63) = -3;   coil(67:69) = -3;
+
+pos   = (7:42);
+coil   = circshift(coil,step);
+
+for i = 1:36
+   ii = ismember(mesh.tnum, pos(i));
+   J_ele(ii) = coil(i);
+end
+
+J(mesh.elems2nodes(J_ele ==  1)) =  Ja;
+J(mesh.elems2nodes(J_ele == -1)) = -Ja;
+J(mesh.elems2nodes(J_ele ==  2)) =  Jc;
+J(mesh.elems2nodes(J_ele == -2)) = -Jc;
+J(mesh.elems2nodes(J_ele ==  3)) =  Jb;
+J(mesh.elems2nodes(J_ele == -3)) = -Jb;
 
 %% Prescribe fixed Air and Iron domains
 
@@ -54,16 +102,6 @@ ii_opt  = ~ii_fix;
  
 phi(ii_fix0)  = 0;
 phi(ii_fix1)  = 1;
-
-%Plot prescribed domains
-% figure;
-% Motor_PlotEdges(params, 1, 'k-', 2);
-% plot(mesh.x_mid(ii_fix0),mesh.y_mid(ii_fix0),'o','MarkerFaceColor','b');
-% plot(mesh.x_mid(ii_fix1),mesh.y_mid(ii_fix1),'ro','MarkerFaceColor','r');
-% axis equal;
-% 
-% PlotData(mesh.x,mesh.y,mesh.elems2nodes,J);
-% Motor_PlotEdges(params,max(J));
 
 end
 
--- Tests/Motor_Test_Linear.m
+++ Tests/Motor_Test_Linear.m
@@ -15,15 +15,26 @@ model.nonlinear = 0;
 rpm = 1500;
 Tp = (pi/6)/((2*pi*rpm)/60);
 
-time = linspace(0,Tp,37);
+time = (0:3*Tp/36:2*Tp);
 
-for i = 1:length(time)/2
+for i = 1:length(time)
+    
+[J,phi] = Motor_MoveCurrent(mesh, params, i-1, time(i));
 
-[J,phi] = Motor_MoveCurrent(mesh, params, -i+1, time(i));
+% % Plot prescribed domains
+% figure;
+% Motor_PlotEdges(params, 1, 'k-', 2);
+% plot(mesh.x_mid(phi==0),mesh.y_mid(phi==0),'o','MarkerFaceColor','b');
+% plot(mesh.x_mid(phi==1),mesh.y_mid(phi==1),'ro','MarkerFaceColor','r');
+% axis equal;
+ 
+% PlotData(mesh.x,mesh.y,mesh.elems2nodes,J);
+% Motor_PlotEdges(params,max(J));
+
 [A, B] = Motor_GetJ(phi, J,  mesh, matrices, params, model);
 
 PlotData(mesh.x,mesh.y,mesh.elems2nodes,A);
-caxis([-20e-4,20e-4]);
+caxis([-5e-4,5e-4]);
 Motor_PlotEdges(params,max(A));
 
 end
